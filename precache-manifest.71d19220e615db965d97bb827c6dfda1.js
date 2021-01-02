ccba28b48167e1454d032c9009dee85f64aa1a04
Fatima Yousif <fyousif30@gmail.com>
2021-01-03 02:41:29 +0500

    Updates

---
+++ precache-manifest.71d19220e615db965d97bb827c6dfda1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd55ab0e55a1b39d8907c93a4e5206c",
+    "url": "/calendarbuzzer/index.html"
+  },
+  {
+    "revision": "e22c79ba2dda907b7fb5",
+    "url": "/calendarbuzzer/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7c3c6386b00071bf9bec",
+    "url": "/calendarbuzzer/static/css/main.5043496d.chunk.css"
+  },
+  {
+    "revision": "e22c79ba2dda907b7fb5",
+    "url": "/calendarbuzzer/static/js/2.cb04f4f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calendarbuzzer/static/js/2.cb04f4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3c6386b00071bf9bec",
+    "url": "/calendarbuzzer/static/js/main.20c86aeb.chunk.js"
+  },
+  {
+    "revision": "e151b3acfbdf180ed3e0",
+    "url": "/calendarbuzzer/static/js/runtime-main.72933222.js"
+  },
+  {
+    "revision": "edd20e445da445316dc9f8757d54a70c",
+    "url": "/calendarbuzzer/static/media/logo1.edd20e44.png"
+  }
+]);
